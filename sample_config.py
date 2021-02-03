40ebfa081e928b739609e6c208b6911deeaaf9f2
m4mallu <renju700@gmail.com>
2021-02-03 13:06:00 +0530

    Update

--- sample_config.py
+++ sample_config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):    
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID"))
+    API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
@@ -13,4 +13,4 @@ class Config(object):
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 1572864000
     MAX_MESSAGE_LENGTH = 4096
-    PRE_FILE_TXT = os.environ.get("PRE_FILE_TXT", "@MovieKeralam. ")
+    PRE_FILE_TXT = os.environ.get("PRE_FILE_TXT")
