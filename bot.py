41a1bd9878daa6e717a3ad998f7492abd6d006d9
m4mallu <renju700@gmail.com>
2021-02-03 12:08:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,15 +5,23 @@
 # Tg Uid        - @kiddilan
 # Channel       - @MovieKeralam
 # ----------------------------------------- #
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 import os
 import pyrogram
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 if bool(os.environ.get("env", False)):
     from sample_config import Config
 else:
     from config import Config
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     intermedia = dict(root="intermedia")
     app = pyrogram.Client(
         "intermediabot",
